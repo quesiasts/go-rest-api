a3108e71a1f05ef9dc127c8653e38bfccbb22847
Quésia <quesiasts@gmail.com>
2022-02-10 19:06:55 -0300

    add docker-compose.yml

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/quesiasts/go-rest-api/database"
 	"github.com/quesiasts/go-rest-api/models"
 	"github.com/quesiasts/go-rest-api/routes"
 )
@@ -12,6 +13,7 @@ func main() {
 		{Id: 1, Nome: "Nome1", Historia: "Historia1"},
 		{Id: 2, Nome: "Nome2", Historia: "Historia2"},
 	}
+	database.ConectaBancoDeDados()
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
---
+++ migration/docker-database-initial.sql
@@ -0,0 +1,9 @@
+create table personalidades(
+    id serial primary key,
+    nome varchar,
+    historia varchar
+);
+
+INSERT INTO personalidades(nome, historia) VALUES 
+('Deodato Petit Wertheimer', 'Sei lá quem foi ele'),
+('Carmela Dutra', 'nunca nem vi');
