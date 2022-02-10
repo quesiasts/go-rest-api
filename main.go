f72e502afb8e125f72c8644a4add813f17f70e09
Quesia Santos <quesia.santos@olist.com>
2022-02-10 16:55:32 -0300

    add setup from api-go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/quesiasts/go-rest-api/models"
+	"github.com/quesiasts/go-rest-api/routes"
+)
+
+func main() {
+	models.Personalidades = []models.Personalidade{
+		{Id: 1, Nome: "Nome1", Historia: "Historia1"},
+		{Id: 2, Nome: "Nome2", Historia: "Historia2"},
+	}
+	fmt.Println("Iniciando o servidor Rest com Go")
+	routes.HandleRequest()
+}
---
+++ routes/routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/quesiasts/go-rest-api/controllers"
+)
+
+func HandleRequest() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.ExibeTodasPersonalidades).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaPersonalidade).Methods("Get")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
