6e6d1b1c12bd8e9dbcd7b2af7e873c4f8362dc6c
Quésia <quesiasts@gmail.com>
2022-02-23 16:08:48 -0300

    modifying readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# go-rest-api
+## Go RestApi
+
+Objetivo deste projeto foi criar uma Api Rest usando Golang e Frameworks
+
+Ferramentas utilizadas:
+
+    - Docker
+    - PostgreSQL
+    - React
+
+Como subir o servidor:
+
+    1 - Primeiro tenha o docker instalado em sua máquina
+    2 - Após isso rode `go mod tidy`
+    3 - Logo após fazer a instalação dos pacotes go, rode:
+    `docker-compose up`
+    4 - Iniciará o banco de dados com o valor:
+    USER - root
+    PASS - root
+    HOST - root
+    5 - Instale os módulos Node abrindo a pasta de Frontend e rodando:
+        - npm version
+        - npm install
+        - npm update
+        - npm start 
+
+Considerações finais:
+
+    - Frontend :3000 / Backend :8000 / BD :5432
+    - CRUD (Create/Read/Update/Delete)
--- routes/routes.go
+++ routes/routes.go
@@ -4,17 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/quesiasts/go-rest-api/controllers"
+	"github.com/quesiasts/go-rest-api/middleware"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.ExibeTodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaPersonalidade).Methods("Get")
 	r.HandleFunc("/api/personalidades", controllers.CriaNovaPersonalidade).Methods("Post")
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaPersonalidade).Methods("Delete")
 	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
